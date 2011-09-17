86eb4a18f11689a9f529920da344134e514353d1
Jeffrey Santos <jeffrey.m.santos@gmail.com>
2011-09-17 08:47:07 -0400

    Initial Creation/Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ForceOrganizer::Application.config.secret_token = '8993f12b8e18c02a1ba91b1695521f0c289038a47bb049304099bc21008bea56eca84399da2b75d1531f142ad3e937037e6a48ff13e689621b178d8b0df8dd64'
